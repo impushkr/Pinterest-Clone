51a001a4306f982282f18b649d5f0ee76d8cbcd3
Pushkar Singh <singhpushkarpss@gmail.com>
2025-10-14 23:05:49 +0530

    databse setup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://127.0.0.1:27017/PinterestDB");
+
+const userSChema=mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,  
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      minlength: 6, // You can adjust security requirement
+    },
+    dp: {
+      type: String, // URL or path to profile picture
+      default: "",  // optional
+    },
+    boards:[],
+    
 });
 
-module.exports = router;
+
+module.exports=mongoose.model("user",userSChema)
