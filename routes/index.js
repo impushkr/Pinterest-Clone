06b400332e8992d5a6194f68ee865153a4d1358a
Pushkar Singh <singhpushkarpss@gmail.com>
2025-10-14 23:45:34 +0530

    authentication and authorisation added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const userModel=require("./users");
+const passport = require("passport")
+const localStrategy=require("passport-local");
+
+passport.use(new localStrategy(userModel.authenticate()))
 
 
 router.get('/', function(req, res, next) {
@@ -7,8 +12,43 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.get('/profile',isLoggedIn,function(req,res,next){
+  res.send('profile');
+})
+
 router.get('/register',function(req,res,next){
   res.render("register")
 })
+
+router.post("/register",function(req,res){
+  const {username,email,fullname}=req.body;
+  const userData=new userModel({username,email,fullname});
+ 
+  userModel.register(userData,req.body.password).then(function(){
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/profile");
+    })
+  })
+})
+
+router.post("/login",passport.authenticate("local",{
+  successRedirect: "/profile",
+  failureRedirect:"/",
+  failureFlash:true
+}),function(req,res){ })
+
+router.get("/logout",function(req,res){
+  req.logout(function(err){
+    if (err){
+      return next (err);
+    }
+    res.redirect('/')
+  })
+})
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated())return next();
+  res.redirect("/")
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const plm=require("passport-local-mongoose")
 
 mongoose.connect("mongodb://127.0.0.1:27017/PinterestDB");
 
@@ -30,8 +31,9 @@ const userSChema=mongoose.Schema({
       default: "",  // optional
     },
     boards:[],
-    
+
 });
 
+userSChema.plugin(plm)
 
 module.exports=mongoose.model("user",userSChema)
